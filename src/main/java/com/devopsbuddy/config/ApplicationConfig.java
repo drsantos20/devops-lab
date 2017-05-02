80373c91784b5edafa0506f79a04511d853084b3
drsantos <drsantos@uoldiveo.com>
2017-05-02 13:42:50 -0300

    fix config issue

--- src/main/java/com/devopsbuddy/config/ApplicationConfig.java
+++ src/main/java/com/devopsbuddy/config/ApplicationConfig.java
@@ -15,7 +15,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan(basePackages = "com.devopsbuddy.backend.persistence.domain.backend")
 @EnableTransactionManagement //transcation management
 @PropertySource("file:///${user.home}/Documents/dev/application-common.properties")
-//@PropertySource("file:///${user.home}/Documentos/dev/application-common.properties")
 public class ApplicationConfig {
 
 }
